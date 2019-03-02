09158092811b866d634037d2cc05c7310610f1ea
Scott H <zenwattage@gmail.com>
2019-03-01 20:20:27 -0800

    update to page

---
+++ arraytesting.js
@@ -0,0 +1,13 @@
+var list = [
+    {item: "Do Homework"},
+    {item: "Buy Groceries"}
+]
+
+let newList = list.push({item: "Wash Car"});
+
+
+console.log(list[2]);
+
+for (var i = 0 ; i < list.length; i++) {
+    console.log(list[i]);
+}
